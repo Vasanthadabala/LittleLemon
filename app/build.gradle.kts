4732ec560baae8645929aefb349ce7d659bc1357
adabalasatya <satya1adabala@gmail.com>
2023-08-19 12:09:27 +0530

    Updating

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,8 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.navigation:navigation-compose:2.7.0")
+    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation("androidx.compose.material:material:1.4.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
--- app/src/main/java/com/example/littlelemon/HomeScreen.kt
+++ app/src/main/java/com/example/littlelemon/HomeScreen.kt
@@ -1,5 +1,8 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.littlelemon
 
+import android.annotation.SuppressLint
 import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
@@ -9,10 +12,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,82 +33,143 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(navController: NavHostController)
 {
-    Column {
-        UpperScreen()
-        LowerScreen()
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Image(painter = painterResource(id = R.drawable.logo ),
+                        contentDescription = "Logo",
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .size(90.dp)
+                            .padding(vertical = 20.dp, horizontal = 25.dp))
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Menu",
+                            modifier = Modifier.size(32.dp)
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { navController.navigate(Profile.route) }) {
+                        Icon(
+                            imageVector = Icons.Filled.AccountCircle,
+                            contentDescription = "Profile",
+                            modifier = Modifier.size(32.dp)
+                        )
+                    }
+                }
+            )
+        }
+    ) {
+        Column(Modifier.padding(top = 50.dp)) {
+            UpperScreen()
+            LowerScreen()
+        }
     }
-
 }
 
 @Composable
-fun UpperScreen()
-{
+fun UpperScreen() {
     val context = LocalContext.current
-    val state by remember {
-        mutableStateOf(0)
-    }
+    val state by remember { mutableStateOf(0) }
     Column(
         Modifier
             .fillMaxWidth()
-            .background(color = Color(0XFF495E57))
-            .padding(10.dp),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.Start
-    ) {
-        Text(text = "LittleLemon",
-            fontSize = 25.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Yellow,
-            modifier = Modifier.padding(10.dp))
-        Text(text = "Chicago",
-            fontSize = 24.sp,
-            fontWeight = FontWeight.W400,
-            color = Color.White,
-            modifier = Modifier.padding(10.dp))
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .padding(top = 10.dp),
-            horizontalArrangement = Arrangement.Start)
-        {
-            Text(text = stringResource(id = R.string.description),
-                fontSize = 16.sp,
-                fontWeight = FontWeight.W400,
+            .background(color = Color(0XFF41544E))
+            .padding(8.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            Text(
+                text = "Little lemon",
+                fontSize = 34.sp,
+                fontWeight = FontWeight.W800,
+                style = TextStyle(
+                    shadow = Shadow(
+                        color = Color.Black,
+                        offset = Offset(8f, 8f),
+                        blurRadius = 5f
+                    )
+                ),
+                fontFamily = FontFamily.Serif,
+                color = Color.Yellow,
+                modifier = Modifier.padding(start = 10.dp, top = 10.dp, bottom = 2.dp)
+            )
+            Text(
+                text = "Chicago",
+                fontSize = 25.sp,
+                fontWeight = FontWeight.W600,
+                fontFamily = FontFamily.Serif,
                 color = Color.White,
-                modifier = Modifier
-                    .fillMaxWidth(0.6F)
-                    .padding(10.dp, end = 20.dp))
-            Image(painter = painterResource(id = R.drawable.heroimage),
-                contentDescription = "Restaurant Image",
-                modifier = Modifier
-                    .padding(5.dp)
-                    .clip(RoundedCornerShape(12.dp)))
-        }
-        Button(onClick = { Toast.makeText(context,"Order Received", Toast.LENGTH_SHORT).show()},
-            shape = RoundedCornerShape(15.dp),
-            border = BorderStroke(1.dp, Color.Black),
-            colors = ButtonDefaults.buttonColors(Color.Black),
-            modifier = Modifier.padding(10.dp, bottom = 15.dp)) {
-            Text(text = "Reservation",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.W400,
-                modifier = Modifier.padding(5.dp))
-        }
+                modifier = Modifier.padding(start = 10.dp, top = 2.dp, bottom = 5.dp)
+            )
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            )
+            {
+                Text(
+                    text = stringResource(id = R.string.description),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.W500,
+                    fontFamily = FontFamily.SansSerif,
+                    color = Color.White,
+                    modifier = Modifier
+                        .fillMaxWidth(0.6F)
+                        .padding(10.dp, end = 12.dp)
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.heroimage),
+                    contentDescription = "Restaurant Image", contentScale = ContentScale.FillWidth,
+                    modifier = Modifier
+                        .padding(bottom = 10.dp, end = 15.dp)
+                        .size(135.dp)
+                        .clip(RoundedCornerShape(15.dp))
+                )
+            }
+            Button(
+                onClick = { Toast.makeText(context, "Order Received", Toast.LENGTH_SHORT).show() },
+                shape = RoundedCornerShape(12.dp),
+                border = BorderStroke(1.dp, Color.Black),
+                colors = ButtonDefaults.buttonColors(Color.Black),
+                modifier = Modifier.padding(10.dp, bottom = 10.dp)
+            ) {
+                Text(
+                    text = "Reservation",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.W400,
+                    modifier = Modifier.padding(5.dp)
+                )
+            }
 
+        }
     }
-}
 
 @Composable
 fun LowerScreen()
