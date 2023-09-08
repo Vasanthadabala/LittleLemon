706ba72f4264b40834e6629526bec45face3ab56
VasanthAdabala <vasanthadabala@gmail.com>
2023-09-08 15:00:47 +0530

    updateing

---
+++ app/src/main/java/com/example/littlelemon/DataBase.kt
@@ -0,0 +1,33 @@
+package com.example.littlelemon
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.RoomDatabase
+
+@Database(entities = [MenuItemEntity::class], version = 1, exportSchema = false)
+abstract class AppDataBase:RoomDatabase()
+{
+    abstract fun menuDao():MenuDao //connecting dao interface to function so we can access and perform operations menu items.
+}
+@Dao
+interface MenuDao{
+    @Query("SELECT * FROM menu_items")
+    suspend fun getAllMenuItems():LiveData<List<MenuItemEntity>>//
+    @Insert
+    suspend fun insertAll(menuItems: List<MenuItemEntity>)
+}
+
+@Entity(tableName = "menu_items")
+data class MenuItemEntity(
+    @PrimaryKey val id: Int,
+    val title: String,
+    val description: String,
+    val price: Double,
+    val image: String
+)
--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -14,9 +14,51 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.room.Room
 import com.example.littlelemon.ui.theme.LittleLemonTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
 class MainActivity : ComponentActivity() {
+
+    private val client = HttpClient(Android) {
+        install(ContentNegotiation) {
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+            }
+            )
+        }
+    }
+    private  suspend fun fetchMenuData():List<MenuItemNetwork>
+    {
+        return client.get("https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json")
+            .body<MenuNetworkData>().menu // Declaring End point for accesing data
+    }
+    
+    private val database by lazy{
+        Room.databaseBuilder(applicationContext,AppDataBase::class.java,"MenuDataBase-1").build()
+        //Stores Instance of Room database(more like dbms->database which manages the data of room)
+    }
+
+    private suspend fun SaveMenuToLocalDatabase(menuItems:List<MenuItemNetwork>){
+        val menuItemsRoom = menuItems.map {
+            MenuItemEntity(
+                id = it.id,
+                title = it.title,
+                description = it.description,
+                price = it.price,
+                image = it.image
+            )
+        }
+        database.menuDao().insertAll(menuItemsRoom)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
